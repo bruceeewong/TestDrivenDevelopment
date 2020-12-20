e9cc604683f20c4a3a72d31a420f5b988bf2e5ba
bruski <292803281@qq.com>
2020-12-20 22:45:33 +0800

    refactor: eliminate subclass

--- src/MultiCurrencyMoney/Money.java
+++ src/MultiCurrencyMoney/Money.java
@@ -1,7 +1,17 @@
 package MultiCurrencyMoney;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
