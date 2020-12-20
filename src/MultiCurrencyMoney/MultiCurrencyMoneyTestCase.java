a3a57f731903d3216253c5ee4f575568b23c3eab
bruski <292803281@qq.com>
2020-12-20 23:26:21 +0800

    refactor: remove duplication of dollar & france

--- src/MultiCurrencyMoney/MultiCurrencyMoneyTestCase.java
+++ src/MultiCurrencyMoney/MultiCurrencyMoneyTestCase.java
@@ -18,7 +18,7 @@ public class MultiCurrencyMoneyTestCase {
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = Money.franc(5);
+        Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
@@ -28,8 +28,6 @@ public class MultiCurrencyMoneyTestCase {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -37,10 +35,5 @@ public class MultiCurrencyMoneyTestCase {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
