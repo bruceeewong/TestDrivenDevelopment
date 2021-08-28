75a59aaa559f8548fcc2e63e992363ee2347f5f0
bruski <292803281@qq.com>
2021-08-28 21:10:38 +0800

    feat(ch8-making_objects): git rid of duplication of times()

--- src/MultiCurrencyMoney/Money.java
+++ src/MultiCurrencyMoney/Money.java
@@ -7,8 +7,20 @@ public class Money {
         this.amount = amount;
     }
 
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && this.getClass() == object.getClass();
+    }
+
+    public Money times(int amount) {
+        return new Dollar(amount);
     }
 }
