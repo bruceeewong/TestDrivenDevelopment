3618d37ca660a3922c504626e6c0ba82a4c30118
bruski <292803281@qq.com>
2021-08-28 21:38:02 +0800

    feat(ch11-the_root_of_all_evil): use factory mode to eliminate all the sub classes

--- src/MultiCurrencyMoney/Money.java
+++ src/MultiCurrencyMoney/Money.java
@@ -13,12 +13,12 @@ public class Money {
         this.currency = currency;
     }
 
-    static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
@@ -27,7 +27,7 @@ public class Money {
     }
 
     public Money times(int multiplier) {
-        return null;
+        return new Money (amount * multiplier, currency);
     };
 
     @Override
